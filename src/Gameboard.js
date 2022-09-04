e52b64adbbfdd1e514fb97b71e1f093fefa052a1
Piotr Fijoł <piotr.fijol1@gmail.com>
2022-09-04 11:09:39 +0200

    Encapsulate gameboard state in Gameboard factory function

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,7 +1,7 @@
 export default function Gameboard(gameboardSize) {
     
     let _gridSize = gameboardSize;
-    let grid = [];
+    let _grid = [];
     
     const initGrid = () => {
         for(let i=0; i<3; i++) {
@@ -9,12 +9,22 @@ export default function Gameboard(gameboardSize) {
             for(let j=0; j<3; j++) {
                 row.push('');
             }
-            grid.push(row);
+            _grid.push(row);
         }
     }
 
     const getGridSize = () => {
         return _gridSize;
+    }
+
+
+    const setMarkerAtPosition = (x, y, marker) => {
+        if(_grid[x][y] !== '') {
+            _grid[x][y] = marker; 
+            return true;
+        }
+        
+        return false;
     }
 
     initGrid();
