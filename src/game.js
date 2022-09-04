cec58ade525437eb75b22653c04c63fdcafd87d5
Piotr Fijoł <piotr.fijol1@gmail.com>
2022-09-04 12:44:24 +0200

    Implement makeMove function

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 import Player from './Player.js';
 import Gameboard from './Gameboard.js';
+import * as displayController from './displayController.js';
 
 let _players = [];
 let _gameboard;
@@ -17,6 +18,35 @@ function getRound() {
     return _round;
 }
 
+function initGameboard(gridSize) {
+    let gameboard = new Gameboard(gridSize);
+    displayController.renderBoard(gameboard.getGrid());
+
+    return gameboard;
+}
+
+function makeMove(x, y, marker) {
+    let playerMoved = _gameboard.setMarkerAtPosition(x, y, marker);
+    if(!playerMoved) {
+        console.error("Cant overwrite other player's marker");
+    } else {
+        let scenario = checkScenario(x, y, marker);
+        switch(scenario) {
+            case 1:
+                //Tie
+                break;
+            case 2:
+                //Win
+                break;
+            default:
+                break;
+        }
+        
+    }
+}
+
+function checkScenario(x, y, marker) {}
+
 function startGame() {
     let markers = ['x',' o'];  
     let playerMarker = Math.floor(Math.random() * 2);   
@@ -29,17 +59,7 @@ function startGame() {
     
     let gridSize = 3;
     _gameboard = initGameboard(gridSize);
-
 }
-
-function initGameboard(gridSize) {
-    let gameboard = new Gameboard(gridSize);
-    displayController.renderBoard(gameboard.getState());
-
-    return gameboard;
-}
-
-
 
 export {
     startGame,
