e2373cebc1c00a3a8f4ad18a71a1184da5b14df6
Piotr Fijoł <piotr.fijol1@gmail.com>
2022-09-05 12:21:43 +0200

    Refactor code

--- src/game.js
+++ src/game.js
@@ -1,17 +1,17 @@
 import Player from './Player.js';
 import Gameboard from './Gameboard.js';
 import * as displayController from './displayController.js';
+import {AIPlayer, UNBEATABLE, EASY} from './AIPlayer.js';
 
+
+let AI;
 
 let _players = [];
 let _gameboard;
 let _round = 0;
 
 let gridSize = 3;
-
-function getGameboard() {
-    return _gameboard;
-}
+let markers = ['x','o'];  
 
 function newRound() {
     _round++;
@@ -22,15 +22,19 @@ function getRound() {
 }
 
 function initGameboard(gridSize) {
-    let gameboard = new Gameboard(gridSize);
+    let gameboard = Gameboard(gridSize);
+    renderBoard(gameboard);
+    
+    return gameboard;
+}
+
+function renderBoard(gameboard) {
     displayController.renderBoard(gameboard.getGrid(), ev => {
         makeMove(
             ev.currentTarget.dataset.position % 3, 
             parseInt(ev.currentTarget.dataset.position / 3),
             _players[1].getMarker())
     });
-
-    return gameboard;
 }
 
 function restartGame() {
@@ -43,15 +47,12 @@ function restartGame() {
 function makeMove(x, y, marker) {
     let playerMoved = _gameboard.setMarkerAtPosition(x, y, marker);
     if(!playerMoved) {
-        console.error("Cant overwrite other player's marker");
+        if(marker !== AI.getMarker()) {
+            console.error("Cant overwrite other player's marker");
+        }
         return false;
     } else {
-        displayController.renderBoard(_gameboard.getGrid(), ev => {
-            makeMove(
-                ev.currentTarget.dataset.position % 3, 
-                parseInt(ev.currentTarget.dataset.position / 3),
-                _players[1].getMarker())
-        });
+        renderBoard(_gameboard);
 
         let scenario = checkScenario(getRound(), _gameboard.getGrid());
 
@@ -61,7 +62,7 @@ function makeMove(x, y, marker) {
                 gameOver();
                 break;
             case 2:
-                let msg = marker === _players[1].getMarker() ? 'You won! Send me a screenshot ;o' : 'AI won.. come on man';
+                let msg = marker === _players[1].getMarker() ? 'You won! Congratulations.' : 'AI won.. come on man';
                 displayController.renderMessage(msg);
                 gameOver();
                 break;
@@ -69,12 +70,16 @@ function makeMove(x, y, marker) {
         newRound();
         
         //if Computer's move
-        if(_round % 2 === 0) {
+        if(currentMarker() === AI.getMarker()) {
             AIMakeDecision();
         }
     }
 
     return true;
+}
+
+function currentMarker() {
+    return markers[getRound()%2];
 }
 
 function gameOver() {
@@ -95,38 +100,40 @@ function emptySpots(board) {
         }
     }
 
-    return result;;
+    return result;
 }
 
 function AIMakeDecision() {
-    /*
-    let decision = {
-        x: Math.floor(Math.random() * _gameboard.getGridSize()),
-        y: Math.floor(Math.random() * _gameboard.getGridSize())
-    }
-    
-    while(!makeMove(decision.x, decision.y, _players[1].getMarker())) {
-        decision = {
-            x: Math.floor(Math.random() * _gameboard.getGridSize()),
-            y: Math.floor(Math.random() * _gameboard.getGridSize())
+    let decision;
+    if(AI.difficulty === EASY) {
+        function randomChoice() {
+            return {
+                x: Math.floor(Math.random() * _gameboard.getGridSize()),
+                y: Math.floor(Math.random() * _gameboard.getGridSize())
+            }
         }
+        decision = randomChoice();
+        
+        while(!makeMove(decision.x, decision.y, _players[0].getMarker())) {
+            decision = randomChoice()
+        }
+    } else if(AI.difficulty === UNBEATABLE) {
+        decision = minimax(getRound(), _gameboard.getGrid(), true, 0);
+        makeMove(decision.x, decision.y, AI.getMarker())
+        
     }
-    console.log(decision)
-*/
 
-    
-    let decision = minimax(getRound(), _gameboard.getGrid(), true, 0);
 
     function minimax(round, board, isMax, depth) {
         let freeSpots = emptySpots(board);
         let marker = _players[round%2].getMarker();
-
         let moveResult = checkScenario(Math.max(0, round-1), board);
+
         // Look for termial nodes
         if(moveResult === 1) {
             return {value: 0};
         } else if (moveResult === 2) {
-            return {value: isMax ? -50+depth : 0-depth};
+            return {value: isMax ? -10+depth : 10-depth};
         }
 
         if(isMax) {
@@ -171,46 +178,41 @@ function AIMakeDecision() {
             return val;
         }
     }
+}
 
-    makeMove(decision.x, decision.y, _players[0].getMarker())
+function checkScenario(round, state) {
+    let marker = _players[round%2].getMarker();
 
+    // Check rows;
+    if(state.some(row => row.every(field => field === marker))) {
+        return 2;
     }
 
-    function checkScenario(round, state) {
-        let marker = _players[round%2].getMarker();
-
-        // Check rows;
-        if(state.some(row => row.every(field => field === marker))) {
-            return 2;
-        }
-
-        //Check cols;
-        if(state[0].some((col, colID) => state.every(row => row[colID] === marker))) {
-            return 2;
-        }
-
-        //Check diagonals
-        if(!state.some((row, index) => row[index] !== marker)) {
-            return 2;
-        } else if(!state.some((row, index) => row[2-index] !== marker)) {
-            return 2;
-        } 
-
-        if((round+1) === (_gameboard.getGridSize() ** 2)) return 1;
-
-        return 0;
+    //Check cols;
+    if(state[0].some((col, colID) => state.every(row => row[colID] === marker))) {
+        return 2;
     }
+
+    //Check diagonals
+    if(!state.some((row, index) => row[index] !== marker)) {
+        return 2;
+    } else if(!state.some((row, index) => row[2-index] !== marker)) {
+        return 2;
+    } 
+
+    if((round+1) === (_gameboard.getGridSize() ** 2)) return 1;
+
+    return 0;
+}
 
 function startGame() {
-    let markers = ['x','o'];  
     let playerMarker = Math.floor(Math.random() * 2);   
 
+    let playerOne = Player(markers[playerMarker]);
+
     // Assign to the AI player a marker that is still free
-    let computer  = new Player(markers[(playerMarker + 1) % markers.length]);    
-
-    let playerOne = new Player(markers[playerMarker]);
-
-    _players.push(computer, playerOne);
+    AI = AIPlayer(markers[(playerMarker + 1) % markers.length], EASY);
+    _players.push(AI, playerOne);
     
     _gameboard = initGameboard(gridSize);
     AIMakeDecision();
